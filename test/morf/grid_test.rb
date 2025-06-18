60cf1cda971714a9bad5f519b1bca28ed4696e8a
Nebojša Stričević <strika@hackberry.dev>
2025-06-18 07:32:21 +0200

    Implement Grid initialization

---
+++ test/morf/grid_test.rb
@@ -0,0 +1,14 @@
+require "morph/grid"
+
+class CellStub
+end
+
+describe Morph::Grid do
+  describe "initialization" do
+    it "initializes a grid of cells with the correct number of rows and columns" do
+      grid = Grid.new(cell: CellStub, rows: 4, columns: 8)
+
+      _(grid.cell(row: 2, column: 7).instance_of?(CellStub)).must_equal true
+    end
+  end
+end
